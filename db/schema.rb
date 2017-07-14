1ed3cf4fa838d5f6012455c5e9b661d0dc923cac
xyz <xyz@localhost.localdomain>
2017-07-14 18:09:14 +0200

    corret scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714144343) do
+ActiveRecord::Schema.define(version: 20170714160636) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
